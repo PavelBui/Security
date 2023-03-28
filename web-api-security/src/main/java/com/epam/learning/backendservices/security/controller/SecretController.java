545071ae703142a6f59a23e29ec204b034852c69
PavelBui <89442856+PavelBui@users.noreply.github.com>
2023-03-28 12:16:49 +0300

    Added solution for 12-13 steps

--- web-api-security/src/main/java/com/epam/learning/backendservices/security/controller/SecretController.java
+++ web-api-security/src/main/java/com/epam/learning/backendservices/security/controller/SecretController.java
@@ -1,10 +1,7 @@
 package com.epam.learning.backendservices.security.controller;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/secret")
 public interface SecretController {
@@ -14,5 +11,8 @@ public interface SecretController {
 
     @PostMapping
     String setSecret(Model model, @RequestParam String secret);
+
+    @GetMapping("/{uuid}")
+    String showSecret(Model model, @PathVariable String uuid);
 
 }
--- web-api-security/src/main/java/com/epam/learning/backendservices/security/controller/impl/SecretControllerImpl.java
+++ web-api-security/src/main/java/com/epam/learning/backendservices/security/controller/impl/SecretControllerImpl.java
@@ -4,6 +4,7 @@ import com.epam.learning.backendservices.security.controller.SecretController;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.net.MalformedURLException;
@@ -16,20 +17,36 @@ public class SecretControllerImpl implements SecretController {
     @Value("${api.base.uri}")
     private URL apiBaseUrl;
 
+    private String secretValue;
+
+    private UUID uuid;
+
     public String getSecretPage() {
         return "secret";
     }
 
     public String setSecret(Model model, @RequestParam String secret) {
-        UUID uuid = UUID.randomUUID();
-        URL url = null;
+        uuid = UUID.randomUUID();
+        secretValue = secret;
+        URL secretUrl = null;
         try {
-            url = new URL(apiBaseUrl, "/secret/" + uuid);
+            secretUrl = new URL(apiBaseUrl, "/secret/" + uuid);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
 
-        model.addAttribute("url", url);
+        model.addAttribute("url", secretUrl);
         return "secretinfo";
     }
+
+    public String showSecret(Model model, @PathVariable String uuid) {
+        if (this.uuid != null && uuid.equals(this.uuid.toString())) {
+            model.addAttribute("secret", secretValue);
+            this.uuid = null;
+            secretValue = "";
+            return "secretvalue";
+        }
+        return "index";
+    }
+
 }
