28b65c0bc2d0398588037ac500666f7f1d87de1c
PavelBui <89442856+PavelBui@users.noreply.github.com>
2023-03-28 10:19:47 +0300

    Refactored

--- web-api-security/src/main/java/com/epam/learning/backendservices/security/controller/impl/SecretControllerImpl.java
+++ web-api-security/src/main/java/com/epam/learning/backendservices/security/controller/impl/SecretControllerImpl.java
@@ -1,14 +1,20 @@
 package com.epam.learning.backendservices.security.controller.impl;
 
 import com.epam.learning.backendservices.security.controller.SecretController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.UUID;
 
 @Controller
 public class SecretControllerImpl implements SecretController {
+
+    @Value("${api.base.uri}")
+    private URL apiBaseUrl;
 
     public String getSecretPage() {
         return "secret";
@@ -16,7 +22,13 @@ public class SecretControllerImpl implements SecretController {
 
     public String setSecret(Model model, @RequestParam String secret) {
         UUID uuid = UUID.randomUUID();
-        String url = "http://localhost8080/secret/" + uuid;
+        URL url = null;
+        try {
+            url = new URL(apiBaseUrl, "/secret/" + uuid);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
         model.addAttribute("url", url);
         return "secretinfo";
     }
